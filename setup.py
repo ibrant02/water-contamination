012c6a382a89e89e2080c3498d4b9b25e74f5489
ibrant02 <brant2302@gmail.com>
2019-07-10 14:51:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import find_packages, setup
 setup(
     name="water_contamination",
     version="1.0.0", 
+    description="Water_Contamination Analysis"
     author="Isaiah Brant",
+    author_email="brant2302@gmail.com"
+    url="https://github.com/ibrant02/water-contamination"
     zip_safe=False
 )
