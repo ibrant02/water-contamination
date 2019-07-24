0f5859e3c01324873e3d81bca71a9d4a66648022
Will Mooney <will.mooney.3@gmail.com>
2019-07-24 12:49:25 -0400

    Add the qsub command builder

---
+++ water_contamination/lib/qsub.py
@@ -0,0 +1,24 @@
+QSUB_ARGUMENTS = ['N', 'cwd', 'pe', 'q', 'S', 'V', 'j', 'b', 'Xmx']
+
+
+def build_command(args, command):
+    """Build the qsub command.
+
+    Args:
+        args: The arguments to this qsub command
+        command (str): The command to run on a Torque scheduler
+
+    Return:
+        the str qsub command that was built
+    """
+    string_list = ["qsub"]
+    for arg in dir(args):
+        if arg.startswith("_") or arg not in QSUB_ARGUMENTS:
+            continue
+        arg_value = getattr(args, arg)
+        if not arg.startswith('X'):
+            string_list.append("-{}={}".format(arg, arg_value))
+        else:
+            string_list.append("--{}={}".format(arg, arg_value))
+    string_list.append(command)
+    return " ".join(string_list)
